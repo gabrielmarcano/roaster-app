06fa7c21dafbb982c0f1d4b028c52e6da216bba3
gabrielmarcano120 <gabrielmarcano120@gmail.com>
2024-11-15 01:24:06 +0000

    disable typescript unused rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@
 module.exports = {
   extends: ['expo', 'prettier'],
   ignorePatterns: ['/dist/*'],
+  // rules: {
+  //   'no-unused-vars': 'off',
+  //   '@typescript-eslint/no-unused-vars': 'off',
+  // }
 };
